3d7af4ef4044711d339eea183db8186923c30784
allen3325 <00857146@email.ntou.edu.tw>
2023-10-11 14:16:29 +0800

    feat: add line notify. doc: update README.md fix: remove useless cookie information in packet.

--- README.md
+++ README.md
@@ -1,15 +1,36 @@
 # 使用方法
-請先登入 iLearning3.0，將 cookie 複製以下資訊：
+請先登入 iLearning3.0，取得以下資訊：
 
-IPCZQX03846e843c: Yours
+- lessons_id: ["lesson_id_1", "lesson_id_2", ...]
+    
+    登陸 iLearning 查看 lesson_id，如圖 22214 為電腦視覺的 id
+    
+    ![alt](https://i.imgur.com/N351HyV.png)
 
-citrix_ns_id: Yours
+- lessons_name: ["lesson_name_1", "lesson_name_2", ...] 
+    
+    填入對應的 lesson_name，例如 lesson_id 為 ["22214", "lesson_id_2", ...] 則 lessons_name 為 ["電腦視覺", "lesson_name_2", ...]
 
-PHPSESSID: Yours
+- PHPSESSID: Yours
+
+    cookie 取得方法:
+
+    F12 -> Tab 選擇 Application -> Storage -> Cookies
+
+    如圖：
+
+    ![alt](https://i.imgur.com/u2e35AO.png)
+
+- line_token: [申請教學](https://tools.wingzero.tw/article/sn/1224)
 
 複製 .env.example，並重新命名為 .env ，將以上資訊填入
 
 # 執行
 ```bash
+pip install -r requirements.txt 
 python craw_lesson.py
+# 可以考慮使用 & 使其背景執行 -> python craw_lesson.py &
 ```
+
+# TODO
+Docker Container 形式執行
--- craw_lesson.py
+++ craw_lesson.py
@@ -1,43 +1,25 @@
-'''
-curl "https://lms2020.nchu.edu.tw/course/homeworkList/24288" ^
-  -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7" ^
-  -H "Accept-Language: zh-TW,zh;q=0.9,en;q=0.8" ^
-  -H "Cache-Control: max-age=0" ^
-  -H "Connection: keep-alive" ^
-  -H "Cookie: PHPSESSID=lrimucrvkfmolaoep4mgnprbc4; locale=zh-tw; noteFontSize=100; noteExpand=0; citrix_ns_id=AAI7-MgKZTsklQAAAAAAADs1q9dOSa1g07j2O40F8FvPVG9hHneWxPw4yT22tvUeOw==DdAKZQ==drQ0q-QZT3SqrXcM4bLelIisIx4=; IPCZQX03846e843c=0100ca008c780df80728a508ed0771175aae222e;timezone=^%^2B0800" ^
-  -H "Referer: https://lms2020.nchu.edu.tw/course/24288" ^
-  -H "Sec-Fetch-Dest: document" ^
-  -H "Sec-Fetch-Mode: navigate" ^
-  -H "Sec-Fetch-Site: same-origin" ^
-  -H "Sec-Fetch-User: ?1" ^
-  -H "Upgrade-Insecure-Requests: 1" ^
-  -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.60" ^
-  -H "sec-ch-ua: ^\^"Microsoft Edge^\^";v=^\^"117^\^", ^\^"Not;A=Brand^\^";v=^\^"8^\^", ^\^"Chromium^\^";v=^\^"117^\^"" ^
-  -H "sec-ch-ua-mobile: ?0" ^
-  -H "sec-ch-ua-platform: ^\^"Windows^\^"" ^
-  --compressed
-  '''
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
 import os
 import json
+import time
+import schedule
 
 load_dotenv()
 
 lessons_id = json.loads(os.getenv('lessons_id'))
 lessons_name = json.loads(os.getenv('lessons_name'))
 PHPSESSID = os.getenv('PHPSESSID')
-citrix_ns_id = os.getenv('citrix_ns_id')
-IPCZQX03846e843c = os.getenv('IPCZQX03846e843c')
+line_token = os.getenv('line_token')
 
 headers = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
     "Accept-Language": "zh-TW,zh;q=0.9,en;q=0.8",
     "Cache-Control": "max-age=0",
     "Connection": "keep-alive",
-    "Cookie": f'PHPSESSID={PHPSESSID}; locale=zh-tw; noteFontSize=100; noteExpand=0; citrix_ns_id={citrix_ns_id}; IPCZQX03846e843c={IPCZQX03846e843c};timezone=^%^2B0800',
+    "Cookie": f'PHPSESSID={PHPSESSID}; locale=zh-tw; noteFontSize=100; noteExpand=0; timezone=^%^2B0800',
     "Referer": "https://lms2020.nchu.edu.tw/course/24288",
     "Sec-Fetch-Dest": "document",
     "Sec-Fetch-Mode": "navigate",
@@ -52,7 +34,22 @@ headers = {
 
 
 def fake_notify(hw_name,target_time):
-   print(f"作業 {hw_name} 即將在 {target_time} 到期")
+  url = "https://notify-api.line.me/api/notify"
+
+  payload = f'message={hw_name}在{target_time}到期'
+  headers = {
+    'Authorization': f'Bearer {line_token}',
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+
+  response = requests.request("POST", url, headers=headers, data=payload)
+
+  time.sleep(1)
+
+  if response.status_code == 200:
+    print("發送成功")
+  else:
+     print(f"發送失敗，作業 {hw_name} 即將在 {target_time} 到期")
 
 def parse_time(tag_text):
   date_string_with_year = "2023-" + tag_text
@@ -69,7 +66,7 @@ def compare_time(hw_name, target_time):
   # current_time = parse_time("10-22 23:59")
   # 比較目標時間和現在時間的差異
   time_difference = target_time - current_time
-  # 如果差異大於2天，調用自定義函數
+  # 如果差異大於3天，調用自定義函數
   if time_difference <= timedelta(days=3):
       return True
   else:
@@ -87,7 +84,6 @@ def check_lesson(lesson_id):
       # HW Name
       if count_element % 7 == 2:
           hw_name = tag.text
-          # print(hw_name)
       # HW Status Check Logo
       if count_element % 7 == 6:
           hw_status_tmp = tag.text
@@ -107,8 +103,22 @@ def check_lesson(lesson_id):
         hw_target_time = parse_time(tag.text)
         should_draw = compare_time(hw_name, hw_target_time)
 
-# check a lesson
-for idx, lesson_id in enumerate(lessons_id): 
-  # print(lesson_id)
-  print(f'{lessons_name[idx]}:')
-  check_lesson(lesson_id)
+def check_task():
+  for idx, lesson_id in enumerate(lessons_id): 
+    print(f'{lessons_name[idx]}:')
+    check_lesson(lesson_id)
+
+# 定義一個函數來安排任務
+def schedule_task():
+    # 每天的00:00和12:00執行my_task函數
+    schedule.every().day.at("00:01").do(check_task)
+    schedule.every().day.at("12:01").do(check_task)
+
+if __name__ == '__main__':
+  # 開始排程
+  schedule_task()
+
+  # 保持腳本運行，以便任務可以被執行
+  while True:
+      schedule.run_pending()
+      time.sleep(1)
